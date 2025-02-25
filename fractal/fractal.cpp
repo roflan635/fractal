5f887105074f1e2af79faeb8cc11ca3aa68d0724
roflan635 <148633870+roflan635@users.noreply.github.com>
2025-02-25 14:17:54 +0400

    Update fractal.cpp
    point надо было перед радиусами во время создания сторон квадрата

--- fractal/fractal.cpp
+++ fractal/fractal.cpp
@@ -1,4 +1,4 @@
-﻿#include "SFML/Graphics.hpp"
+#include "SFML/Graphics.hpp"
 
 using namespace sf; // подключаем пространство имен sf
 
@@ -60,14 +60,14 @@ void StackCircleCollection(float xc, float yc, float radius, int nestingLength)
     Push(StackValue{ xc, yc, radius, nestingLength }); // заносим в стек свойства базовой окружности
     while (!IsEmpty()) // цикл пока стек не пуст
     {
-        auto point = Pop(); // извлекаем координаты и свойства новой окружности из стека
-        RectangleShape rectangle(Vector2f(radius *2, radius *2)); // создаем окружность
+        auto point = Pop(); // извлекаем координаты и свойства нового квадрата из стека
+        RectangleShape rectangle(Vector2f(point.radius*2, point.radius*2)); // создаем окружность
         rectangle.setOrigin(point.radius, point.radius); // устанавливаем точку привязки по центру окружности
         rectangle.setOutlineColor(Color::Black); // цвет границы черный
         rectangle.setFillColor(Color::Transparent); // заливка прозрачная
         rectangle.setOutlineThickness(1); // толщина 1
         rectangle.setPosition(point.x, point.y); // перемещаем окружность в точку xc, yc
-        window.draw(rectangle); // отрисовываем окружность в буфере кадра
+        window.draw(rectangle); // отрисовываем квуадрат в буфере кадра
         if (point.nestingLength > 1) // если глубина вложенности превышает 1, то помещаем в стек данные о 4-х новых окружностях
         {                                                      // располагающихся сверху, снизу, слева и справа от текущей окружности
             Push(StackValue{ point.x - point.radius, point.y - point.radius, point.radius/2, point.nestingLength - 1 });
@@ -95,61 +95,8 @@ int main()
         // Установка цвета фона
         window.clear(Color::White);
         // рисуем наш узор рекурсивной функцией
-        StackCircleCollection(400, 300, 150, 2);
+        StackCircleCollection(400, 300, 150, 4);
         // Отрисовка окна
         window.display();
     }
 }
-
-/*
-#include "SFML/Graphics.hpp"
-
-using namespace sf; // подключаем пространство имен sf
-
-// Объект, который, собственно, является главным окном приложения
-RenderWindow window(VideoMode(800, 600), "Squares");
-
-// xc, yc - координаты центра
-// radius - радиус
-// nestingLength - глубина вложенности рисования
-void SquareCollection(float xc, float yc, float radius, int nestingLength)
-{
-    if (nestingLength == 0) return; // если дошли до 0 вложенности - прекращаем рекурсию
-    RectangleShape rectangle(Vector2f(2 * radius, 2 * radius)); // создаем квадрат
-    rectangle.setOrigin(radius, radius); // устанавливаем точку привязки по центру
-    rectangle.setOutlineColor(Color::Black); // цвет границы черный
-    rectangle.setFillColor(Color::Transparent); // заливка прозрачная
-    rectangle.setOutlineThickness(1); // толщина 1
-    rectangle.setPosition(xc, yc); // перемещаем окружность в точку xc, yc
-    window.draw(rectangle); // отрисовываем окружность в буфере кадра
-    if (nestingLength > 1) // если глубина вложенности превышает 1, то рисуем окружности по сторонам рекурсивно
-    {
-        SquareCollection(xc - radius, yc + radius, radius / 2, nestingLength - 1); // радиусом в 2 раза меньшим
-        SquareCollection(xc - radius, yc - radius, radius / 2, nestingLength - 1);
-        SquareCollection(xc + radius, yc - radius, radius / 2, nestingLength - 1);
-        SquareCollection(xc + radius, yc + radius, radius / 2, nestingLength - 1);
-    }
-}
-
-int main()
-{
-    window.setFramerateLimit(1);
-    while (window.isOpen())
-    {
-        // Обрабатываем очередь событий в цикле
-        Event event;
-        while (window.pollEvent(event))
-        {
-            // Пользователь нажал на «крестик» и хочет закрыть окно?
-            if (event.type == Event::Closed)
-                window.close(); // тогда закрываем его
-        }
-        // Установка цвета фона
-        window.clear(Color::White);
-        // рисуем наш узор рекурсивной функцией
-        SquareCollection(400, 300, 150, 4);
-        // Отрисовка окна
-        window.display();
-    }
-}
-*/
